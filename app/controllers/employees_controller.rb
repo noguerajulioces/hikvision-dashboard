b5fc7db5ad1e53d4fa17ab4b3d1d5ec78eb4aefb
noguerajulioces <noguerajulio24@gmail.com>
2025-01-15 13:37:21 -0300

    update groups

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,6 +1,12 @@
 class EmployeesController < ApplicationController
   def search
     query = params[:query]
+
+    if query.blank?
+      render json: []
+      return
+    end
+
     employees = Employee.where("first_name LIKE ? OR last_name LIKE ?", "%#{query}%", "%#{query}%")
                         .where(group_id: nil)
     render json: employees.select(:id, :first_name, :last_name, :role)
