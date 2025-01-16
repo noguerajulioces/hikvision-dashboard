2b2bf6cac189074d1a1c2bedcbd7ccc2f339f5b4
noguerajulioces <noguerajulio24@gmail.com>
2025-01-16 11:03:51 -0300

    revert select2

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,3 @@ pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "stimulus-password-visibility" # @2.1.1
-pin "select2" # @4.1.0
-pin "jquery" # @3.7.1
