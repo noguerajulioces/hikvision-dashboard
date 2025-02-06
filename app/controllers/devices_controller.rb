4f336a4efbc11a1c6c7c2a8be616f456c73cb1bf
noguerajulioces <noguerajulio24@gmail.com>
2025-02-05 22:34:01 -0300

    agregar device

---
+++ app/controllers/devices_controller.rb
@@ -0,0 +1,58 @@
+class DevicesController < ApplicationController
+  before_action :set_device, only: [:show, :edit, :update, :destroy]
+
+  # GET /devices
+  def index
+    @devices = Device.page(params[:page])
+  end
+
+  # GET /devices/1
+  def show
+  end
+
+  # GET /devices/new
+  def new
+    @device = Device.new
+  end
+
+  # GET /devices/1/edit
+  def edit
+  end
+
+  # POST /devices
+  def create
+    @device = Device.new(device_params)
+
+    if @device.save
+      redirect_to @device, notice: 'Dispositivo creado exitosamente.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /devices/1
+  def update
+    if @device.update(device_params)
+      redirect_to @device, notice: 'Dispositivo actualizado exitosamente.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /devices/1
+  def destroy
+    @device.destroy
+    redirect_to devices_url, notice: 'Dispositivo eliminado exitosamente.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions
+    def set_device
+      @device = Device.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through
+    def device_params
+      params.require(:device).permit(:name, :ip_address, :model)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -58,4 +58,5 @@ Rails.application.routes.draw do
   end
 
   resources :payrolls
+  resources :devices
 end
