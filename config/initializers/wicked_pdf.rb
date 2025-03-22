08b12c6328b63b0010d7f8078aca54f0dbf0ad5d
noguerajulioces <noguerajulio24@gmail.com>
2025-03-22 11:55:13 -0300

    refactor(wicked_pdf): simplify configuration by removing comments and using hash syntax
    The configuration for WickedPdf was refactored to use a hash syntax instead of a block, making it more concise and easier to read. Unnecessary comments were removed to streamline the file, focusing only on the essential configuration for the wkhtmltopdf executable path. This change improves maintainability and reduces clutter in the configuration file.

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -2,29 +2,23 @@
 #
 # Use this to set up shared configuration options for your entire application.
 # Any of the configuration options shown here can also be applied to single
-# models by passing arguments to the `render :pdf` call.
-#
-# To learn more, check out the README:
-#
-# https://github.com/mileszs/wicked_pdf/blob/master/README.md
+# controller by copying the appropriate line in a before_action.
 
-WickedPdf.configure do |config|
-  # Path to the wkhtmltopdf executable: This usually isn't needed if using
-  # one of the wkhtmltopdf-binary family of gems.
-  config.exe_path = "/usr/bin/wkhtmltopdf"
-  #   or
-  # config.exe_path = Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
-
-  # Needed for wkhtmltopdf 0.12.6+ to use many wicked_pdf asset helpers
-  # config.enable_local_file_access = true
-
-  # Layout file to be used for all PDFs
-  # (but can be overridden in `render :pdf` calls)
-  # config.layout = 'pdf.html'
-
-  # Using wkhtmltopdf without an X server can be achieved by enabling the
-  # 'use_xvfb' flag. This will wrap all wkhtmltopdf commands around the
-  # 'xvfb-run' command, in order to simulate an X server.
-  #
-  # config.use_xvfb = true
-end
+WickedPdf.config = {
+  # Path to the wkhtmltopdf executable: This will work on macOS with Homebrew or on Ubuntu
+  exe_path: (
+    if Gem.win_platform?
+      # Windows
+      'C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe'
+    elsif File.exist?('/usr/local/bin/wkhtmltopdf')
+      # macOS (Homebrew)
+      '/usr/local/bin/wkhtmltopdf'
+    elsif File.exist?('/usr/bin/wkhtmltopdf')
+      # Ubuntu/Debian
+      '/usr/bin/wkhtmltopdf'
+    else
+      # Try to find it in PATH
+      Gem.win_platform? ? 'wkhtmltopdf.exe' : 'wkhtmltopdf'
+    end
+  )
+}
